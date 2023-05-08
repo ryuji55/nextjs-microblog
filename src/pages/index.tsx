bd555daa3c8b30edeb2e7a37e20fe75a4c6c5119
ryuji55 <rt.october.2@icloud.com>
2023-05-08 17:47:06 +0900

    Fix:レイアウト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import Link from "next/link";
 import Layout from "@/components/layout";
 import utilStyles from "../styles/utils.module.css";
+import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,12 @@ export default function Home() {
       </section>
       <section>
         <div>エンジニアのブログ</div>
-        <div>
+        {/* 以下がブログ表示 */}
+        <div className={styles.grid}>
           <article>
             <Link href="/">
               <Image
+                className={styles.thumbnailImage}
                 src="/images/thumbnail01.jpg"
                 alt=""
                 width={100}
@@ -26,10 +29,66 @@ export default function Home() {
               />
             </Link>
             <Link href="/">
-              <p>SSGとSSRの使う場面はいつなのか？</p>
+              <p className={utilStyles.boldText}>
+                SSGとSSRの使う場面はいつなのか？
+              </p>
             </Link>
             <br />
-            <small>may 8, 2023</small>
+            <small className={utilStyles.lightText}>may 8, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <Image
+                className={styles.thumbnailImage}
+                src="/images/thumbnail01.jpg"
+                alt=""
+                width={100}
+                height={100}
+              />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>
+                SSGとSSRの使う場面はいつなのか？
+              </p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>may 8, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <Image
+                className={styles.thumbnailImage}
+                src="/images/thumbnail01.jpg"
+                alt=""
+                width={100}
+                height={100}
+              />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>
+                SSGとSSRの使う場面はいつなのか？
+              </p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>may 8, 2023</small>
+          </article>
+          <article>
+            <Link href="/">
+              <Image
+                className={styles.thumbnailImage}
+                src="/images/thumbnail01.jpg"
+                alt=""
+                width={100}
+                height={100}
+              />
+            </Link>
+            <Link href="/">
+              <p className={utilStyles.boldText}>
+                SSGとSSRの使う場面はいつなのか？
+              </p>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>may 8, 2023</small>
           </article>
         </div>
       </section>
